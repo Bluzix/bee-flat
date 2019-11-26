2e7c8c04dfaa3c3b6b54987761e5f6f753e02190
jedulz <quirozjesus10@gmail.com>
2019-11-25 23:46:45 -0500

    Created a moving character
    foundation for a moving player
    player moves with keyboard controls

---
+++ main.js
@@ -0,0 +1,93 @@
+let canvas = document.getElementById('canvas');
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+let ctx = canvas.getContext('2d');
+console.log(canvas)
+
+//incase we want to stop an animation frame
+let animateId;
+let player;
+
+
+class Player{
+    constructor(x,y,width,height){
+        this.x = x;
+        this.y = y;
+        this.dx = 0;
+        this.dy = 0;
+        this.width = width;
+        this.height = height;
+        this.speed = 3;
+        this.up = false;
+        this.down = false;
+        this.left = false;
+        this.right = false;
+    }
+
+    draw(){
+        ctx.beginPath();//you have to begin a new path everytime
+        ctx.fillStyle = '#ffdc34';
+        ctx.rect(this.x,this.y,this.width,this.height);
+        ctx.fill();
+    }
+
+    update(){
+        this.x += this.dx;
+        this.y += this.dy;
+    }
+}
+
+function update(){
+    player.update();
+}
+
+function draw(){
+    player.draw();
+}
+
+function animate(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    update();
+    draw();
+    animateId = requestAnimationFrame(animate);
+}
+
+function init(){
+    player = new Player(10,10,10,20);
+    animate();
+};
+
+document.addEventListener('keydown', function(e){
+    if(e.key == "ArrowUp" && !player.up){
+        player.dy -= player.speed;
+        player.up = true;
+    }else if(e.key == "ArrowDown" && !player.down){
+        player.dy += player.speed;
+        player.down = true;
+    }else if(e.key == "ArrowLeft" && !player.left){
+        player.dx -= player.speed;
+        player.left = true;
+    }else if(e.key == "ArrowRight" && !player.right){
+        player.dx += player.speed;
+        player.right = true;
+    }
+});
+
+document.addEventListener('keyup', function(e){
+    if(e.key == "ArrowUp" && player.up){
+        player.dy += player.speed;
+        player.up = false;
+    }else if(e.key == "ArrowDown" && player.down){
+        player.dy -= player.speed;
+        player.down = false;
+    }else if(e.key == "ArrowLeft" && player.left){
+        player.dx += player.speed;
+        player.left = false;
+    }else if(e.key == "ArrowRight" && player.right){
+        player.dx -= player.speed;
+        player.right = false;
+    }
+});
+
+init();
